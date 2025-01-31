6abb87e77c08757e79eaa00ac14b78688f3e36e2
Nihat774 <nihatmemmedov.0520@gmail.com>
2025-01-31 12:37:34 -0800

    added title and robot.txt

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/about",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/contact",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
